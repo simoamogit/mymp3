4d96b94bc961a0145f1b622ac43554128d3e5a1d
simoamogit <simoneinver@gmail.com>
2025-06-20 14:40:57 +0200

    Add files via upload

--- src/components/PlayerControls.jsx
+++ src/components/PlayerControls.jsx
@@ -6,19 +6,26 @@ import StopIcon from '@mui/icons-material/Stop';
 import { Howler } from 'howler';
 
 function PlayerControls() {
-  return (
-    <Stack direction="row" spacing={2} sx={{ mt: 2 }}>
-      <Button variant="contained" color="primary" startIcon={<StopIcon />} onClick={() => Howler.stop()}>
-        Stop
-      </Button>
-      <Button variant="contained" color="primary" startIcon={<PlayArrowIcon />} onClick={() => Howler._howls[0]?.play()}>
-        Play
-      </Button>
-      <Button variant="contained" color="primary" startIcon={<PauseIcon />} onClick={() => Howler._howls[0]?.pause()}>
-        Pause
-      </Button>
-    </Stack>
-  );
+    const handlePlay = () => {
+        const howl = Howler._howls[0];
+        if (howl && !howl.playing()) {
+            howl.play();
+        }
+    };
+
+    return (
+        <Stack direction="row" spacing={2} sx={{ mt: 2 }}>
+            <Button variant="contained" color="primary" startIcon={<StopIcon />} onClick={() => Howler.stop()}>
+                Stop
+            </Button>
+            <Button variant="contained" color="primary" startIcon={<PlayArrowIcon />} onClick={handlePlay}>
+                Play
+            </Button>
+            <Button variant="contained" color="primary" startIcon={<PauseIcon />} onClick={() => Howler._howls[0]?.pause()}>
+                Pause
+            </Button>
+        </Stack>
+    );
 }
 
 export default PlayerControls;
